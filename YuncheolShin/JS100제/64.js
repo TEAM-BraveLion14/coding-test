7907a3ae0e7347058371bc30c8c3e32edb62debf
dev-yun <yunpro2766@gmail.com>
2022-11-07 17:37:46 +0900

    [ETC] JS100제 / 64번 수정

--- YuncheolShin/JS100제/64.js
+++ YuncheolShin/JS100제/64.js
@@ -11,15 +11,46 @@
 가장 적게 옮길 수 있는 횟수를 출력합니다.
 만약 어떻게 해도 정량이 N이 되지 않는다면 -1을 출력합니다.*/
 
+//dp
 const weight = parseInt(prompt('무게를 입력하세요.(숫자)'));
 
 let count = 0;
 
-function getTotalCount(count, weight) {
-  result += Math.floor(weight / 7);
-  result += Math.floor((weight % 7) / 3);
+const dp = new Array(weight + 1).fill(0);
+dp[3] = 1;
+dp[7] = 1;
 
-  return (weight % 7) % 3 === 0 ? result : -1;
+for (let i = 3; i < dp.length; i++) {
+  if (i % 7 === 0) {
+    dp[i] = dp[i - 7] + 1;
+  } else if (i % 3 === 0) {
+    dp[i] = dp[i - 3] + 1;
+  } else if ((i % 7) % 3 === 0) {
+    dp[i] = dp[i - 7 * Math.floor(i / 7)] + dp[7 * Math.floor(i / 7)];
+  }
+}
+
+console.log(dp[weight]);
+for (let i = 0; i < dp.length; i++) {
+  console.log(`${i} : ${dp[i]}`);
+}
+
+/**const weight = parseInt(prompt('무게를 입력하세요.(숫자)'));
+
+let count = 0;
+
+function getTotalCount(count, weight) {
+  while (weight > 0) {
+    if (weight % 7 === 0) {
+      count += weight / 7;
+      return count;
+    }
+
+    weight -= 3;
+    count += 1;
+  }
+  return weight === 0 ? count : -1;
 }
 
 console.log(getTotalCount(count, weight));
+*/
