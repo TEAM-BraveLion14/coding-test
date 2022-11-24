4efb5363be364733956a386bbd4fdbc2ffe3f845
dev-yun <yunpro2766@gmail.com>
2022-11-24 21:48:44 +0900

    [ETC] JS100제 84~85번

---
+++ YuncheolShin/JS100제/84.js
@@ -0,0 +1,46 @@
+/**소정이는 어떤 숫자에서 k개의 수를 뽑았을 때 가장 큰 수를 찾는 놀이를 하고 있습니다. 
+예를 들어, 숫자 1723에서 두 개의 수를 뽑으면 [17, 12, 13, 72, 73, 23]을 만들 수 있습니다.
+이 중 가장 큰 수는 73입니다.
+
+위 예시처럼 어떤 수 n에서 k개의 수를 선택하여 만들 수 있는 수 중에서 가장 큰 수를 찾아 주세요. */
+
+// 문제가 조금 이상 => 두개의 수를 뽑으면 17도 가능하고 71도 가능 => 순열
+
+// 방법 1. for문 1123 같은 경우 처리
+// const number = 1723;
+
+// const numStr = String(number);
+// const result = [];
+// for (let i of numStr) {
+//   for (let j of numStr) {
+//     if (i === j) continue;
+//     result.push(parseInt(i + j));
+//   }
+// }
+
+// console.log(result);
+// console.log(Math.max(...result));
+
+// 방법 2. 순열
+const getPermutations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+const number = 1723;
+
+const numStr = String(number).split('');
+
+const permutations = getPermutations(numStr, 2);
+const result = permutations.map((e) => parseInt(e.join('')));
+console.log(result);
+console.log(Math.max(...result));
---
+++ YuncheolShin/JS100제/85.js
@@ -0,0 +1,47 @@
+/**일정한 규칙을 가지고 있는 숫자를 나열하는 놀이를 하는 중입니다.
+이전 숫자에서 각 숫자의 개수를 나타내어 숫자로 만들고 다시 그 숫자를 같은 규칙으로 만들며 나열합니다.
+이 놀이는 1부터 시작합니다.
+다음 수는 1이 1개이기 때문에 '11'이 되고,
+ '11'에서 1이 2개이기 때문에 그다음은 '12'가 됩니다.
+
+즉,
+1. 1  → (1) 
+2. 11 → (1이 1개)
+
+3. 12 → (1이 2개)
+4. 1121 → (1이 1개 2가 1개)
+
+5. 1321 → (1이 3개 2가 1개)
+6. 122131 → (1이 2개 2가 1개 3이 1개)
+
+7. 132231 → (1이 3개 2가 2개 3이 1개)
+8. 122232 -> 
+\
+위와 같이 진행되는 규칙을 통해 진행 횟수 N을 입력받으면 해당되는 수를 출력하세요.*/
+
+// https://www.notion.so/85-9c20bb185f75419f9b8181615b1e157c
+
+// set 으로 숫자의 종류를 뽑아내고
+// 숫자의 종류를 뽑아내서 키를 돌면서 1121 -> 1 2
+// 1 -> "1121".match(/1/g).length
+// key값 + "1121".match(/1/g).length
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+const N = 8;
+let str = '1';
+
+// 인덱스 1부터 시작 고려
+for (let i = 0; i < N - 1; i++) {
+  const uniqueNums = [...new Set(str)].sort((a, b) => a - b);
+  const tmpArr = [];
+
+  uniqueNums.forEach((e) => {
+    const reg = new RegExp(e, 'g');
+    const matched = str.match(reg);
+    tmpArr.push(e + matched.length);
+  });
+
+  str = tmpArr.join('');
+}
+
+console.log(str);
